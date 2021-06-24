26164af4605ef30b33595f1148d3724ff936ac3a
Darya Tlepbergenova <tdaryat@gmail.com>
2021-06-24 17:26:01 +0300

    write simple shutdown program

---
+++ main.py
@@ -0,0 +1,46 @@
+import paramiko
+import sys
+
+#get public IP address from command line
+try:
+    hostname = sys.argv[1]
+except IndexError: 
+    print("Enter public IP address")
+    sys.exit(1)
+#standart name of user    
+myuser   = 'ec2-user'
+#path for private key
+key = "d:\\Users\\user\\Downloads\\test.pem"
+#list of commands
+commands = [ "ls", "sudo shutdown -h now" ]
+
+k = paramiko.RSAKey.from_private_key_file(key)
+c = paramiko.SSHClient()
+c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+print("connecting....")
+try:
+    #connect
+    c.connect( hostname = hostname, username = myuser, pkey = k)
+    print("connected!")
+    for command in commands:
+        print("Executing {}".format( command ))
+        stdin , stdout, stderr = c.exec_command(command)
+
+        #printing
+        output = ''
+        outputErr = ''
+        for line in stdout.readlines():
+            output += line
+        
+        for line in stderr.readlines():
+            outputErr += line
+        
+        if output:
+            print(output)
+        else:
+            print("There was no output for this command")
+        if outputErr:
+            print(outputErr)
+    c.close()
+except TimeoutError:
+    print("TimeoutError: " + "maybe you alrady close your ec2 or you dont fix your public IP")
